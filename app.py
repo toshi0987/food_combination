878ba95932fc4fb5523266aef559bdab4076ba94
Ohshima <junyandadao702@gmail.com>
2021-12-02 09:37:23 +0900

    add code

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ uri=os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri=uri.replace("postgres://","postgresql://",1)
 app.config['SQLALCHEMY_DATABASE_URI'] =uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 ###データベース用クラス####
